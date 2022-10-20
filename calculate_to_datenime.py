f7daf5b812fbdb1984cdb35087837268dab0524c
Артем Дитинбир <ArtyDir@gmail.com>
2022-10-20 21:08:40 +0700

    вывод результата в удобном формате

--- calculate_to_datenime.py
+++ calculate_to_datenime.py
@@ -7,9 +7,4 @@ hours = n // 3600 % 24
 minutes = n % 3600 // 60
 seconds = n % 3600 % 60
 
-print(f"Годы:    {years}\n"
-      f"Месяцы:  {mounts}\n"
-      f"Дни:     {days}\n"
-      f"Часы:    {hours}\n" 
-      f"Минуты:  {minutes}\n"
-      f"Секунды: {seconds}")
+print(f"{years} лет, {mounts} месяцев, {days} дней, {hours:02}:{minutes:02}:{seconds:02}")
